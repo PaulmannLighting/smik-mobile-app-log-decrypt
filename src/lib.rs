7bb4540d29e24bd99d208330b5427f2e051e8b08
Richard Neumann <neumann@paulmann.de>
2024-01-31 09:40:48 +0100

    Implement common CLI

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+use clap::{builder::PossibleValue, ValueEnum};
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum LogType {
+    Android,
+    Ios,
+}
+
+impl LogType {
+    /// Decrypts the respective log file.
+    ///
+    /// # Errors
+    /// Returns an [`anyhow::Error`] if decryption fails.
+    pub fn decrypt(&self, ciphertext: &str, key: &[u8]) -> anyhow::Result<Vec<u8>> {
+        match self {
+            Self::Android => android_log_decrypt::decrypt(ciphertext, key),
+            Self::Ios => {
+                let mut bytes = Vec::new();
+                for block in
+                    ios_log_decrypt::EncryptedLog::new(ciphertext.into()).decrypt(key.into())
+                {
+                    match block {
+                        Ok(block) => bytes.extend(block),
+                        Err(error) => return Err(error),
+                    }
+                }
+                Ok(bytes)
+            }
+        }
+    }
+}
+
+impl ValueEnum for LogType {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[Self::Android, Self::Ios]
+    }
+
+    fn to_possible_value(&self) -> Option<PossibleValue> {
+        Some(match self {
+            Self::Android => PossibleValue::new("android"),
+            Self::Ios => PossibleValue::new("ios"),
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use clap::Parser;
+use clap_stdin::FileOrStdin;
+use log::error;
+use rpassword::prompt_password;
+use smik_mobile_app_log_decrypt::LogType;
+use std::io::{stdout, BufWriter, Write};
+use std::process::exit;
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg(index = 1, help = "the type of log file to decrypt")]
+    log_type: LogType,
+    #[arg(index = 2, help = "path to the encrypted log file")]
+    logfile: FileOrStdin<String>,
+    #[arg(long, short, help = "hexadecimal decryption key")]
+    key: Option<String>,
+}
+
+impl Args {
+    #[must_use]
+    pub fn hex_key(&self) -> String {
+        self.key.clone().unwrap_or_else(|| {
+            prompt_password("Decryption key: ").unwrap_or_else(|error| {
+                error!("{error}");
+                exit(1)
+            })
+        })
+    }
+
+    pub fn key(&self) -> Vec<u8> {
+        hex::decode(self.hex_key()).unwrap_or_else(|error| {
+            error!("{error}");
+            exit(2);
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let args = Args::parse();
+    BufWriter::new(stdout().lock())
+        .write_all(
+            &args
+                .log_type
+                .decrypt(
+                    &args.logfile.clone().contents().unwrap_or_else(|error| {
+                        error!("{error}");
+                        exit(3)
+                    }),
+                    &args.key(),
+                )
+                .unwrap_or_else(|error| {
+                    error!("{error}");
+                    exit(4);
+                }),
+        )
+        .unwrap_or_else(|error| {
+            error!("{error}");
+            exit(5);
+        });
 }
