0caac00239fab663fd626a5795314bbc66e4449f
Richard Neumann <neumann@paulmann.de>
2024-02-05 09:10:53 +0100

    Use new ios API

--- src/lib.rs
+++ src/lib.rs
@@ -14,22 +14,7 @@ impl LogType {
     pub fn decrypt(&self, ciphertext: &str, key: &[u8]) -> anyhow::Result<Vec<u8>> {
         match self {
             Self::Android => android_log_decrypt::decrypt(ciphertext, key),
-            Self::Ios => {
-                let encrypted = ios_log_decrypt::EncryptedLog::new(ciphertext.into());
-                let blocks: Vec<anyhow::Result<Vec<u8>>> = encrypted.decrypt(key.into()).collect();
-                let mut bytes = Vec::with_capacity(
-                    blocks
-                        .iter()
-                        .map(|result| result.as_ref().map(Vec::len).unwrap_or(0))
-                        .sum(),
-                );
-
-                for block in blocks {
-                    bytes.extend(block?);
-                }
-
-                Ok(bytes)
-            }
+            Self::Ios => ios_log_decrypt::decrypt(ciphertext, key),
         }
     }
 }
